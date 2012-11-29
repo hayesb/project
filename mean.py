0b3bc922e3f9a78d312b067fc56a0a14bb965a66
Barrie Hayes <bhayes@email.unc.edu>
2012-11-29 14:23:17 -0500

    Handled division by zero and added program purpose comment

--- mean.py
+++ mean.py
@@ -1,6 +1,11 @@
+# print error if array is empty except for name
 # mean.py
 import sys
 sum = 0
+
+if len(sys.argv) == 1:
+	print 'Error: No arguments given.'
+	sys.exit()
 for num in sys.argv[1:]:
     sum += float(num)
 print sum / (len(sys.argv) - 1)
